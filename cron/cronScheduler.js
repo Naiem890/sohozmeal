c17d1fbf40f16cfc5ff7d6a6d6e940435df067ae
Solaiman Islam Naiem <59079456+Naiem890@users.noreply.github.com>
2023-09-17 21:41:24 +0600

    corn job code refactor

--- cron/cronScheduler.js
+++ cron/cronScheduler.js
@@ -1,7 +1,46 @@
 const cron = require("node-cron");
-const generateMeals = require("./generateMeal");
+const Meal = require("../src/models/meal");
 
 // Schedule the cron job to run daily at 10:00 PM (adjust as needed)
-cron.schedule("* 22 * * *", generateMeals);
+cron.schedule("* 22 * * *", async () => {
+  // Your cron job logic goes here
+  console.log("Cron job executed: Generating meals for students.");
+
+  try {
+    // Get the current date
+    const currentDate = new Date(); // 17
+
+    currentDate.setDate(currentDate.getDate() + 1); // 18
+
+    // Retrieve meals for the previous day
+    const previousDayMeals = await Meal.find({
+      date: currentDate.toISOString().split("T")[0],
+    });
+
+    // Calculate the next day
+    currentDate.setDate(currentDate.getDate() + 1); //19
+
+    // Format the next day as a string (e.g., "yyyy-mm-dd")
+    const nextDay = currentDate.toISOString().split("T")[0];
+
+    if (previousDayMeals.length > 0) {
+      // Create new meals for the current day by copying data from previous day meals
+      const newMeals = previousDayMeals.map((meal) => ({
+        ...meal.toObject(),
+        _id: undefined, // Exclude the _id field to create a new document
+        date: nextDay, // Set the current date
+      }));
+
+      // Insert the new meals into the database
+      await Meal.insertMany(newMeals);
+
+      console.log("New meals created successfully!", nextDay);
+    } else {
+      console.log("No meals found for the previous day.");
+    }
+  } catch (error) {
+    console.error("Error generating meal:");
+  }
+});
 
 console.log("Cron job scheduled to run daily at 10:00 PM.");
