129b4db80556b103b36e03e26fc6ce838cf2c63f
Solaiman Islam Naiem <59079456+Naiem890@users.noreply.github.com>
2023-09-18 22:29:06 +0600

    corn testing

--- cron/cronScheduler.js
+++ cron/cronScheduler.js
@@ -3,7 +3,7 @@ const Meal = require("../src/models/meal");
 
 // Schedule the cron job to run daily at 10:00 PM (adjust as needed)
 cron.schedule(
-  "20 22 * * *",
+  "30 22 * * *",
   async () => {
     // Your cron job logic goes here
     console.log("Cron job executed: Generating meals for students.");
