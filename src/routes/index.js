ae91e8ee614195e7bdb3b511a37964026c500c50
Solaiman Islam Naiem <59079456+Naiem890@users.noreply.github.com>
2023-09-20 23:38:10 +0600

    cron testing

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,25 @@ const router = require("express").Router();
 const authController = require("../controllers/authController");
 const studentController = require("../controllers/studentController");
 const mealController = require("../controllers/mealController");
+const { sendSMS } = require("../utils/sendSMS");
 
 router.use("/auth", authController);
 router.use("/student", studentController);
 router.use("/meal", mealController);
+
+// write a test route to check the sms functionality
+router.get("/test-sms", async (req, res) => {
+  try {
+    const result = await sendSMS(
+      "Message working from Sohoz Meal App",
+      "01790732717"
+    );
+    console.log("SMS sent successfully!", result);
+    res.status(200).json({ message: "SMS sent successfully!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "An error occurred" });
+  }
+});
 
 module.exports = router;
