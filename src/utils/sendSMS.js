a8397bf5cfa7d2f0fed2bc7d41b56258639c4207
Solaiman Islam Naiem <59079456+Naiem890@users.noreply.github.com>
2023-09-21 00:11:34 +0600

    cron testing

--- src/utils/sendSMS.js
+++ src/utils/sendSMS.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const sendSMS = async (phone, message) => {
+const sendSMS = async (message, phone) => {
   // eslint-disable-next-line no-undef
   const apiKey = process.env.API_KEY;
   // eslint-disable-next-line no-undef
@@ -13,9 +13,13 @@ const sendSMS = async (phone, message) => {
     throw new Error("Phone number or message is missing");
   }
 
-  return await axios.post(
-    `http://bulksmsbd.net/api/smsapi?api_key=${apiKey}&type=text&number=phone&senderid=${senderId}&message=${message}`
-  );
+  return await axios.post(`http://bulksmsbd.net/api/smsapi`, {
+    api_key: apiKey,
+    type: "text",
+    number: phone,
+    senderid: senderId,
+    message: message,
+  });
 };
 
 module.exports = { sendSMS };
