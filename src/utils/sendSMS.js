ef4c89e8bbe32e676b805b431b281f70c56731ad
Solaiman Islam Naiem <59079456+Naiem890@users.noreply.github.com>
2023-09-20 23:48:39 +0600

    cron testing

--- src/utils/sendSMS.js
+++ src/utils/sendSMS.js
@@ -5,7 +5,8 @@ const sendSMS = async (phone, message) => {
   const apiKey = process.env.API_KEY;
   // eslint-disable-next-line no-undef
   const senderId = process.env.SENDER_ID;
-
+  console.log("apiKey", apiKey);
+  console.log("senderId", senderId);
   if (!apiKey || !senderId) {
     throw new Error("API_KEY or SENDER_ID is missing");
   } else if (!phone || !message) {
