3ea08f60b60968a8e27df4bfb436bb7e27191e9f
Solaiman Islam Naiem <59079456+Naiem890@users.noreply.github.com>
2023-09-24 18:30:49 +0600

    get meal sorted by date

--- src/controllers/mealController.js
+++ src/controllers/mealController.js
@@ -1,12 +1,44 @@
 const router = require("express").Router();
 const Meal = require("../models/meal");
+const Student = require("../models/student");
 const { validateToken } = require("../utils/validateToken");
 
 router.get("/plan", validateToken, async (req, res) => {
   const { studentId } = req.user;
   console.log("studentId", studentId);
   try {
-    const meals = await Meal.find({ studentId });
+    // sort meal with dates
+
+    const meals = await Meal.aggregate([
+      {
+        $match: { studentId: studentId }, // Your match criteria here
+      },
+      {
+        $addFields: {
+          dateAsInt: {
+            $toInt: {
+              $dateToString: {
+                date: {
+                  $dateFromString: {
+                    dateString: "$date",
+                    format: "%Y-%m-%d",
+                  },
+                },
+                format: "%Y%m%d",
+              },
+            },
+          },
+        },
+      },
+      {
+        $sort: { dateAsInt: 1 }, // Sort by date in ascending order
+      },
+      {
+        $project: {
+          dateAsInt: 0, // Exclude the temporary field from the result
+        },
+      },
+    ]);
 
     res.status(200).json({ message: "Meals retrieved successfully", meals });
   } catch (error) {
@@ -50,6 +82,37 @@ router.put("/plan/:mealId", validateToken, async (req, res) => {
   } catch (error) {
     console.error("Error updating meal:", error);
     res.status(500).json({ message: "An error occurred while updating meal" });
+  }
+});
+
+router.post("/generate-meal", async (req, res) => {
+  const { date } = req.body;
+  try {
+    // Fetch all students' studentId
+    const students = await Student.find({}, { studentId: 1 });
+    if (students.length === 0) {
+      return res.status(404).json({ message: "No students found" });
+    }
+
+    const studentsIds = students.map((student) => student.studentId);
+    // Create a meal for each student
+    const mealPromises = studentsIds.map(async (studentId) => {
+      const meal = new Meal({
+        studentId,
+        date,
+      });
+      return await meal.save();
+    });
+
+    // Wait for all meal creation promises to resolve
+    const savedMeals = await Promise.all(mealPromises);
+
+    res.json({ message: "Meal generated successfully", savedMeals });
+  } catch (error) {
+    console.error("Error generating meal:", error);
+    res
+      .status(500)
+      .json({ message: "An error occurred while generating meal" });
   }
 });
 
