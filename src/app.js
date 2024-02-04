a3317696735507c6134103896269a63417f68ac9
Solaiman Islam Naiem <solaiman.naiem890@gmail.com>
2024-02-05 03:53:59 +0600

    small change

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(middleware);
 
 // Default route
 app.get("/", (req, res) => {
-  res.send("Welcome to the Sohoz Meal App!");
+  res.send("Welcome to the Sohoz Meal!");
 });
 
 // Api routes
