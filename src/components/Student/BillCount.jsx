c55ffdaeec7d10ee303bdaa883a9fec76c5637c4
Md Enamul Hoque Marzun <117737824+marzun9620@users.noreply.github.com>
2024-02-22 02:39:28 +0600

    Working with student bill

--- src/components/Student/BillCount.jsx
+++ src/components/Student/BillCount.jsx
@@ -73,110 +73,106 @@ export default function BillCount() {
         </div>
       </div>
       <div className="divider"></div>
-      {mealBillData?.length === 0 && (
-        <div className="md:mt-4">
-          <div className="overflow-x-auto max-h-screen overflow-y-scroll px-1">
-            <table className="table-auto min-w-full divide-y divide-gray-200 shadow-md">
-              <thead className="bg-white shadow-sm sticky top-0 border-0 h-12">
-                <tr className="text-sm text-left font-thin text-gray-500">
-                  <th className="pl-2">Date</th>
-                  <th>Breakfast</th>
-                  <th>Lunch</th>
-                  <th>Dinner</th>
-                  <th>Total Cost</th>
-                </tr>
-              </thead>
-              <tbody>
-                {daysOfMonth.map((day) => {
-                  const billData = mealBillData.find(
-                    (item) => item.date === day
-                  );
-                  if (billData) {
-                    billData.mealBill.breakfast.status
-                      ? (totalBill += billData.mealBill.breakfast.perHeadCost)
-                      : 0;
-                    billData.mealBill.lunch.status
-                      ? (totalBill += billData.mealBill.lunch.perHeadCost)
-                      : 0;
-                    billData.mealBill.dinner.status
-                      ? (totalBill += billData.mealBill.dinner.perHeadCost)
-                      : 0;
-                  }
-                  return (
-                    <tr key={day} className="hover:bg-gray-100">
-                      <td className="pl-2 py-1">
-                        {new Date(day).toLocaleDateString("en-UK")}
-                      </td>
-                      {billData ? (
-                        <>
-                          <td
-                            className={`${
-                              billData.mealBill.breakfast.status
-                                ? "text-green-600 font-bold"
-                                : "text-red-600 font-bold"
-                            }`}
-                          >
-                            {billData.mealBill.breakfast.perHeadCost.toFixed(2)}{" "}
-                            ৳
-                          </td>
-                          <td
-                            className={`${
-                              billData.mealBill.lunch.status
-                                ? "text-green-600 font-bold"
-                                : "text-red-600 font-bold"
-                            }`}
-                          >
-                            {billData.mealBill.lunch.perHeadCost.toFixed(2)} ৳
-                          </td>
-                          <td
-                            className={`${
-                              billData.mealBill.dinner.status
-                                ? "text-green-600 font-bold"
-                                : "text-red-600 font-bold"
-                            }`}
-                          >
-                            {billData.mealBill.dinner.perHeadCost.toFixed(2)} ৳
-                          </td>
-                          <td>
-                            {(billData.mealBill.breakfast.status
-                              ? billData.mealBill.breakfast.perHeadCost
-                              : 0 + billData.mealBill.lunch.status
-                              ? billData.mealBill.lunch.perHeadCost
-                              : 0 + billData.mealBill.dinner.status
-                              ? billData.mealBill.dinner.perHeadCost
-                              : 0
-                            ).toFixed(2)}{" "}
-                            ৳
-                          </td>
-                        </>
-                      ) : (
-                        <>
-                          <td>0.00 ৳</td>
-                          <td>0.00 ৳</td>
-                          <td>0.00 ৳</td>
-                          <td>0.00 ৳</td>
-                        </>
-                      )}
-                    </tr>
-                  );
-                })}
-                {/* Grand Total Row */}
-                <tr className="">
-                  <td className="font-bold text-xl pl-2 pt-2">Grand Total</td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td>
-                    <span className="font-bold text-xl">
-                      {totalBill.toFixed(2)} ৳
-                    </span>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+      <div className="md:mt-4">
+        <div className="overflow-x-auto max-h-screen overflow-y-scroll px-1">
+          <table className="table-auto min-w-full divide-y divide-gray-200 shadow-md">
+            <thead className="bg-white shadow-sm sticky top-0 border-0 h-12">
+              <tr className="text-sm text-left font-thin text-gray-500">
+                <th className="pl-2">Date</th>
+                <th>Breakfast</th>
+                <th>Lunch</th>
+                <th>Dinner</th>
+                <th>Total Cost</th>
+              </tr>
+            </thead>
+            <tbody>
+              {daysOfMonth.map((day) => {
+                const billData = mealBillData.find((item) => item.date === day);
+                if (billData) {
+                  billData.mealBill.breakfast.status
+                    ? (totalBill += billData.mealBill.breakfast.perHeadCost)
+                    : 0;
+                  billData.mealBill.lunch.status
+                    ? (totalBill += billData.mealBill.lunch.perHeadCost)
+                    : 0;
+                  billData.mealBill.dinner.status
+                    ? (totalBill += billData.mealBill.dinner.perHeadCost)
+                    : 0;
+                }
+                return (
+                  <tr key={day} className="hover:bg-gray-100">
+                    <td className="pl-2 py-1">
+                      {new Date(day).toLocaleDateString("en-UK")}
+                    </td>
+                    {billData ? (
+                      <>
+                        <td
+                          className={`${
+                            billData.mealBill.breakfast.status
+                              ? "text-green-600 font-bold"
+                              : "text-red-600 font-bold"
+                          }`}
+                        >
+                          {billData.mealBill.breakfast.perHeadCost.toFixed(2)} ৳
+                        </td>
+                        <td
+                          className={`${
+                            billData.mealBill.lunch.status
+                              ? "text-green-600 font-bold"
+                              : "text-red-600 font-bold"
+                          }`}
+                        >
+                          {billData.mealBill.lunch.perHeadCost.toFixed(2)} ৳
+                        </td>
+                        <td
+                          className={`${
+                            billData.mealBill.dinner.status
+                              ? "text-green-600 font-bold"
+                              : "text-red-600 font-bold"
+                          }`}
+                        >
+                          {billData.mealBill.dinner.perHeadCost.toFixed(2)} ৳
+                        </td>
+                        <td>
+                          {(billData.mealBill.breakfast.status
+                            ? billData.mealBill.breakfast.perHeadCost
+                            : 0 + billData.mealBill.lunch.status
+                            ? billData.mealBill.lunch.perHeadCost
+                            : 0 + billData.mealBill.dinner.status
+                            ? billData.mealBill.dinner.perHeadCost
+                            : 0
+                          ).toFixed(2)}{" "}
+                          ৳
+                        </td>
+                      </>
+                    ) : (
+                      <>
+                        <td>0.00 ৳</td>
+                        <td>0.00 ৳</td>
+                        <td>0.00 ৳</td>
+                        <td>0.00 ৳</td>
+                      </>
+                    )}
+                  </tr>
+                );
+              })}
+              {/* Grand Total Row */}
+              <tr className="">
+                <td className="font-bold text-xl pl-2 pt-2">Grand Total</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                  <span className="font-bold text-xl">
+                    {totalBill.toFixed(2)} ৳
+                  </span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-      )}
+      </div>
+      <div className="md:mt-16"></div>
     </div>
   );
 }
