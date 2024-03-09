146d6d7eba5559592fba9fd2af8d48a1e14c8aaf
shohanshovo11 <101856535+shohanshovo11@users.noreply.github.com>
2024-03-09 22:42:04 +0600

    design fix for bill

--- src/components/Student/BillCount.jsx
+++ src/components/Student/BillCount.jsx
@@ -4,29 +4,10 @@ import formatDate from "../../Utils/formatDateString";
 import { Axios } from "../../api/api";
 
 export default function BillCount() {
-  const [student, setStudent] = useState(null);
-  const [name, setName] = useState("");
   const [distinctMonths, setDistinctMonths] = useState([]);
   const [selectedMonth, setSelectedMonth] = useState("");
   const [mealBillData, setMealBillData] = useState([]);
-
-  const fetchStudentProfile = async () => {
-    try {
-      const res = await Axios.get("/student");
-      if (res?.data?.student) {
-        setStudent(res.data.student);
-        setName(res.data.student.name);
-      } else {
-        console.log("Student data not found");
-      }
-    } catch (err) {
-      console.log("Error fetching student data:", err);
-    }
-  };
-
-  useEffect(() => {
-    fetchStudentProfile();
-  }, []);
+  let totalBill = 0;
 
   useEffect(() => {
     const fetchDistinctMonths = async () => {
@@ -46,7 +27,6 @@ export default function BillCount() {
           const res = await Axios.get(
             `/bill/student?year=${year}&month=${month}`
           );
-          console.log("Response data:", res.data);
           setMealBillData(res.data.mealBillData);
         } catch (err) {
           console.log("Error fetching bill data:", err);
@@ -56,61 +36,50 @@ export default function BillCount() {
     fetchBill();
   }, [selectedMonth]);
 
-  const handleMonthChange = useCallback(
-    (increment) => {
-      setSelectedMonth((prevMonth) => {
-        const index = distinctMonths.indexOf(prevMonth);
-        const newIndex = index + increment;
-        return distinctMonths[
-          newIndex >= 0 && newIndex < distinctMonths.length ? newIndex : index
-        ];
-      });
-    },
-    [distinctMonths]
+  const handleMonthChange = useCallback((newMonth) => {
+    setSelectedMonth(newMonth);
+  }, []);
+
+  // Generate an array of dates for the selected month
+  const getDaysArray = (year, month) => {
+    const numDays = new Date(year, month, 0).getDate();
+    return Array.from(
+      { length: numDays },
+      (_, i) => new Date(year, month - 1, i + 2).toISOString().split("T")[0]
+    );
+  };
+
+  // Get the array of days for the selected month
+  const daysOfMonth = getDaysArray(
+    parseInt(selectedMonth.split("-")[0]),
+    parseInt(selectedMonth.split("-")[1])
   );
 
   return (
     <div className="lg:my-10 mb-10 px-5 lg:mr-12">
-      <h2 className="text-3xl font-semibold">
-        Hello, <span className="font-green">{name.split(" ")[0]}</span>, Your
-        Meal Bill Count:
-      </h2>
-      <div className="divider"></div>
-      <div className="flex justify-between items-center mb-6 gap-10 flex-wrap">
-        <div className="flex justify-center items-center gap-10 md:mx-0 mx-auto">
-          <button
-            className={`${
-              selectedMonth === distinctMonths[0]
-                ? "opacity-25 pointer-events-none cursor-not-allowed disabled"
-                : ""
-            }`}
-            onClick={() => handleMonthChange(-1)}
+      <div className="flex justify-between">
+        <h2 className="text-3xl font-semibold">Mess Bill:</h2>
+        <div className="md:mx-0 w-44">
+          <select
+            value={selectedMonth}
+            onChange={(e) => handleMonthChange(e.target.value)}
+            className="rounded-lg p-2 outline-none pr-4 w-full"
           >
-            <ArrowLeftIcon className="h-6 w-6" />
-          </button>
-          <div>
-            <h2 className="text-xl font-semibold">
-              {formatDate(selectedMonth)}
-            </h2>
-          </div>
-          <button
-            className={`${
-              selectedMonth === distinctMonths.slice(-1)[0]
-                ? "opacity-25 pointer-events-none cursor-not-allowed disabled"
-                : ""
-            }`}
-            onClick={() => handleMonthChange(1)}
-          >
-            <ArrowRightIcon className="h-6 w-6" />
-          </button>
+            {distinctMonths.map((month) => (
+              <option key={month} value={month}>
+                {formatDate(month)}
+              </option>
+            ))}
+          </select>
         </div>
       </div>
-      <div className="md:mt-16">
+      <div className="divider"></div>
+      <div className="md:mt-4">
         <div className="overflow-x-auto max-h-screen overflow-y-scroll px-1">
-          <table className="table table-sm table-hover w-full">
+          <table className="table-auto min-w-full divide-y divide-gray-200 shadow-md">
             <thead className="bg-white shadow-sm sticky top-0 border-0 h-12">
-              <tr className="">
-                <th>Date</th>
+              <tr className="text-sm text-left font-thin text-gray-500">
+                <th className="pl-2">Date</th>
                 <th>Breakfast</th>
                 <th>Lunch</th>
                 <th>Dinner</th>
@@ -118,83 +87,88 @@ export default function BillCount() {
               </tr>
             </thead>
             <tbody>
-              {mealBillData.map((item, index) => (
-                <React.Fragment key={item.date}>
-                  <tr className="hover:shadow-sm rounded-lg hover:bg-emerald-50 transition-all border-b-0">
-                    <td className="font-bold text-xl">
-                      {new Date(item.date).toLocaleDateString("en-US", {
-                        month: "short",
-                        day: "numeric",
-                      })}
+              {daysOfMonth.map((day) => {
+                const billData = mealBillData.find((item) => item.date === day);
+                if (billData) {
+                  billData.mealBill.breakfast.status
+                    ? (totalBill += billData.mealBill.breakfast.perHeadCost)
+                    : 0;
+                  billData.mealBill.lunch.status
+                    ? (totalBill += billData.mealBill.lunch.perHeadCost)
+                    : 0;
+                  billData.mealBill.dinner.status
+                    ? (totalBill += billData.mealBill.dinner.perHeadCost)
+                    : 0;
+                }
+                return (
+                  <tr key={day} className="hover:bg-gray-100">
+                    <td className="pl-2 py-1">
+                      {new Date(day).toLocaleDateString("en-UK")}
                     </td>
-                    <td
-                      style={{
-                        color: item.mealBill.breakfast.status ? "red" : "green",
-                      }}
-                    >
-                      <span className="font-bold text-xl">
-                        {item.mealBill.breakfast.perHeadCost} ৳
-                      </span>
-                    </td>
-                    <td
-                      style={{
-                        color: item.mealBill.lunch.status ? "red" : "green",
-                      }}
-                    >
-                      <span className="font-bold text-xl">
-                        {item.mealBill.lunch.perHeadCost} ৳
-                      </span>
-                    </td>
-                    <td
-                      style={{
-                        color: item.mealBill.dinner.status ? "red" : "green",
-                      }}
-                    >
-                      <span className="font-bold text-xl">
-                        {item.mealBill.dinner.perHeadCost} ৳
-                      </span>
-                    </td>
-                    <td>
-                      <span className="font-bold text-xl">
-                        {(
-                          item.mealBill.breakfast.perHeadCost +
-                          item.mealBill.lunch.perHeadCost +
-                          item.mealBill.dinner.perHeadCost
-                        ).toFixed(2)}{" "}
-                        ৳
-                      </span>
-                    </td>
+                    {billData ? (
+                      <>
+                        <td
+                          className={`${
+                            billData.mealBill.breakfast.status
+                              ? "text-green-600 font-bold"
+                              : "text-red-600 font-bold"
+                          }`}
+                        >
+                          {billData.mealBill.breakfast.perHeadCost.toFixed(2)} ৳
+                        </td>
+                        <td
+                          className={`${
+                            billData.mealBill.lunch.status
+                              ? "text-green-600 font-bold"
+                              : "text-red-600 font-bold"
+                          }`}
+                        >
+                          {billData.mealBill.lunch.perHeadCost.toFixed(2)} ৳
+                        </td>
+                        <td
+                          className={`${
+                            billData.mealBill.dinner.status
+                              ? "text-green-600 font-bold"
+                              : "text-red-600 font-bold"
+                          }`}
+                        >
+                          {billData.mealBill.dinner.perHeadCost.toFixed(2)} ৳
+                        </td>
+                        <td>
+                          {(billData.mealBill.breakfast.status
+                            ? billData.mealBill.breakfast.perHeadCost
+                            : 0 + billData.mealBill.lunch.status
+                            ? billData.mealBill.lunch.perHeadCost
+                            : 0 + billData.mealBill.dinner.status
+                            ? billData.mealBill.dinner.perHeadCost
+                            : 0
+                          ).toFixed(2)}{" "}
+                          ৳
+                        </td>
+                      </>
+                    ) : (
+                      <>
+                        <td>0.00 ৳</td>
+                        <td>0.00 ৳</td>
+                        <td>0.00 ৳</td>
+                        <td>0.00 ৳</td>
+                      </>
+                    )}
                   </tr>
-                  {index !== mealBillData.length - 1 && (
-                    <tr className="border-b">
-                      <td colSpan="5"></td>
-                    </tr>
-                  )}
-                </React.Fragment>
-              ))}
-              {mealBillData.length > 0 && (
-                <tr className="border-t">
-                  <td className="font-bold text-xl">Grand Total</td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td>
-                    <span className="font-bold text-xl">
-                      {mealBillData
-                        .reduce(
-                          (total, item) =>
-                            total +
-                            item.mealBill.breakfast.perHeadCost +
-                            item.mealBill.lunch.perHeadCost +
-                            item.mealBill.dinner.perHeadCost,
-                          0
-                        )
-                        .toFixed(2)}{" "}
-                      ৳
-                    </span>
-                  </td>
-                </tr>
-              )}
+                );
+              })}
+              {/* Grand Total Row */}
+              <tr className="">
+                <td className="font-bold text-xl pl-2 pt-2">Grand Total</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                  <span className="font-bold text-xl">
+                    {totalBill.toFixed(2)} ৳
+                  </span>
+                </td>
+              </tr>
             </tbody>
           </table>
         </div>
