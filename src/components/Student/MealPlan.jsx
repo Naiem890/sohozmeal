1f763f127ddcd7e57094980a4a2bde96f0e79d30
shohanshovo11 <101856535+shohanshovo11@users.noreply.github.com>
2024-03-10 07:26:25 +0600

    fix: meal permission check on mealPlan

--- src/components/Student/MealPlan.jsx
+++ src/components/Student/MealPlan.jsx
@@ -16,7 +16,6 @@ export default function MealPlan() {
     const fetchDistinctMonths = async () => {
       const res = await Axios.get("/meal/months");
       const months = res.data;
-      console.log("months", months);
       setDistinctMonths(months);
       setSelectedMonth(months.slice(-1)[0]);
     };
@@ -188,7 +187,9 @@ export default function MealPlan() {
                         }
                         type="checkbox"
                         id={`_checkbox-${meal._id}-${mealType}`}
-                        className={`${meal.permission[i] ? "locked" : ""}`}
+                        className={`${
+                          meal.permission && meal?.permission[i] ? "locked" : ""
+                        }`}
                       />
                       <label htmlFor={`_checkbox-${meal._id}-${mealType}`}>
                         <div className="tick_mark"></div>
