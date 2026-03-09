4825c3a7882f3a151046810c285d5076e791f240
Shohan Shovo <shohanur.shovo@technonext.com>
2026-03-09 15:18:03 +0600

    chore: auto-switch API base URL by environment

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-// export const BASE_URL = "http://localhost:5000/api";
-export const BASE_URL = "https://hall.mist.ac.bd/api";
+export const BASE_URL = import.meta.env.PROD
+  ? "https://hall.mist.ac.bd/api"
+  : "http://localhost:5000/api";
 
 export const Axios = axios.create({ baseURL: BASE_URL });
 
