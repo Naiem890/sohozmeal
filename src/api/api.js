a2e6320630a553bb5d9bbac16bb571e31cf15030
Shohan Shovo <shohanur.shovo@technonext.com>
2026-03-09 15:25:15 +0600

    chore: add .env with VITE_API_URL for environment config

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = import.meta.env.PROD
-  ? "https://hall.mist.ac.bd/api"
-  : "http://localhost:5000/api";
+export const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const Axios = axios.create({ baseURL: BASE_URL });
 
