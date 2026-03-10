45f3937d4abab3cd83a2c3df2f2b6fc13b0d3a59
Shohan Shovo <shohanur.shovo@technonext.com>
2026-03-10 09:38:35 +0600

    feat: migrate routes to TypeScript

---
+++ src/routes/index.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import authController from '../controllers/authController';
+import studentController from '../controllers/studentController';
+import mealController from '../controllers/mealController';
+import stockController from '../controllers/stockController';
+import costController from '../controllers/costController';
+import complaintController from '../controllers/complaintController';
+import hallFeastController from '../controllers/hallFeastController';
+import noticeController from '../controllers/noticeController';
+
+const router = Router();
+
+router.use('/auth', authController);
+router.use('/student', studentController);
+router.use('/meal', mealController);
+router.use('/stock', stockController);
+router.use('/cost', costController);
+router.use('/feast', hallFeastController);
+router.use('/complaint', complaintController);
+router.use('/notice', noticeController);
+
+export default router;
