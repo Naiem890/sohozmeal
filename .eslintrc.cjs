39a513964cdc04cc52203d522295055280910c86
Shohan Shovo <shohanur.shovo@technonext.com>
2026-03-10 09:37:15 +0600

    chore: migrate config and tooling to TypeScript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,20 +3,24 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "@typescript-eslint"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
     "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "off",
     "react/prop-types": "off",
+    "@typescript-eslint/no-explicit-any": "warn",
   },
 };
